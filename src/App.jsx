e3abcfa698aa98d1b1e191680e2d1e6357fc3257
jesusCosio <122650871+jesusCosio@users.noreply.github.com>
2023-01-13 15:37:20 -0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import { Formulario } from "./components/Formulario"
-import { Header } from "./components/Header"
-import { Listado } from "./components/Listado"
-import { useState } from "react";
-
+import { useState } from 'react'
+import './App.css'
+import {Boton} from './components/Boton'
+import { Pantalla } from './components/Pantalla'
 
 function App() {
-  
-  const [alumnos, setAlumnos] = useState([]);
+ 
+  const [valor, setValor] = useState(0);
+
+  const contarClick = () =>{
+    setValor(valor+1);
+  }
+  const reiniciar = () =>{
+    setValor(0);
+  }
 
   return (
-    <div className="container mx-auto mt-10">
-      <Header/>
-      <div className='mt-20 flex'>
-        <Formulario
-        setAlumnos={setAlumnos}/>
-        <Listado/>
-      </div>
+    <div className="App">
+      <Pantalla numero={valor}/>
+      <Boton texto='Click' funcion={contarClick} manejarTipoBtn={true}/>  
+      <Boton texto='Reiniciar' funcion={reiniciar} manejarTipoBtn={false}/>      
     </div>
   )
 }
